1508ae9b15a6ea5e10ae6a41c88aa3923a15317a
fasdser <107477645+fasdser@users.noreply.github.com>
2022-10-14 21:13:08 +0300

    Add project files.

---
+++ DelegateModifier/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateModifier
+{
+    delegate void DelegateModifier();
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listOriginal = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            Func<List<int>> func = () =>
+            {
+                List<int> list = new List<int>();
+                foreach (var item in listOriginal)
+                {
+                    list.Add(item);
+                }
+                return list;
+            };
+            List<int> listNew = func();
+            foreach (var item in listNew)
+            {
+                Console.WriteLine(item);
+            }
+
+            DelegateModifier modifier = () =>
+            {
+                for (int i = listOriginal.Count - 1; i > 0; i--)
+                {
+                    if (listOriginal[i] % 2 != 0)
+                    {
+                        listOriginal[i] = listOriginal[i] + 1;
+
+                    }
+                }
+            };
+
+            modifier();
+            foreach (var item in listOriginal)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ParameterExpression/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace ParameterExpression1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ParameterExpression x = Expression.Parameter(typeof(int), "x");
+
+            var constant1 =Expression.Constant(2);
+            var constant2 =Expression.Constant(3);
+            var constant3 =Expression.Constant(4);
+
+            var subBody = Expression.Subtract(x, constant2);
+            var multBody = Expression.Multiply(constant1, subBody);
+            var addBody = Expression.Add(multBody, x);
+            var body =Expression.Subtract(addBody, constant3);
+
+            var expression = Expression.Lambda<Func<int, int>>(body, x);
+
+            Console.WriteLine(expression.Body);
+
+            Func<int, int> func = expression.Compile();
+
+            Console.WriteLine("Результат: "+func(4));
+            Console.ReadKey();
+        }
+    }
+}
